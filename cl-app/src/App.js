f8d0561f0facb886afb8964de7cba8939fc0f2c9
retropean <eric.randalls@gmail.com>
2019-11-04 14:04:23 -0500

    Create React App + easy template
    Redoing the website in React

---
+++ cl-app/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>findon.cl</h1>
+	  <div>search our database</div>
+	  <form>
+        <input 
+		/>
+        <button type="submit">submit</button>
+      </form>   
+	  <p>
+		or browse by category
+	  </p>
+	  <div>
+		| art | bikes | computers | electronics | vehicles | 
+	  </div>
+	</div>
+  );
+}
+
+export default App;
