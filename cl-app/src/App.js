f98b6d42118e7c3a507dcf7e8a8c5ae0a78c99f5
retropean <eric.randalls@gmail.com>
2019-11-06 13:25:18 -0500

    break out set-up

--- cl-app/src/App.js
+++ cl-app/src/App.js
@@ -1,22 +1,49 @@
 import React from 'react';
 import './App.css';
 
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const Category = () => {
+	return(
+  	  <div>
+		  <p>
+			or browse by category
+		  </p>
+		  <div>
+			| art | bikes | computers | electronics | vehicles | 
+		  </div>
+	  </div>
+	)
+}
+const Header = () => {
+	return(
+  	  <div>
+		  <h1>findon.cl</h1>
+		  <div>search our database</div>
+	  </div>
+	)
+}
+
+const SearchFunction = () => {
+	return(
+  	  <form>
+        <input 
+		/>
+        <Button text='submit' />
+      </form>   
+	)
+}
+
 function App() {
   return (
     <div className="App">
-      <h1>findon.cl</h1>
-	  <div>search our database</div>
-	  <form>
-        <input 
-		/>
-        <button type="submit">submit</button>
-      </form>   
-	  <p>
-		or browse by category
-	  </p>
-	  <div>
-		| art | bikes | computers | electronics | vehicles | 
-	  </div>
+      <Header />
+	  <SearchFunction />
+	<Category />
 	</div>
   );
 }
